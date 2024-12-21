be7660fd8c7ad01d0b545780f8fab7f605b9926e
zenmeraki7 <zenmerakihelp@gmail.com>
2024-12-21 17:44:22 +0530

    updated config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: './frontend', // Set the frontend folder as the root directory for Vite
   plugins: [react()],
-  assetsInclude: ['**/*.html']
+  build: {
+    outDir: '../dist', // Output the build files outside the frontend folder
+    emptyOutDir: true, // Clean the output directory before building
+  }
 })
